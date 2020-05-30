36ec449a4f8367ec6542f53c0a62731e08bf6b68
pal4de <pal4de@gmail.com>
2020-05-30 16:06:08 +0900

    apiだけよそに移した

---
+++ src_server/api.ts
@@ -0,0 +1,20 @@
+// interface Response {
+//     succeeded: boolean,
+//     message: string | null
+// }
+
+// const Event_newEvent = (eventName: string) => {
+// };
+// const Event_deleteEvent = (eventName: string) => {
+// };
+// const Event_changeEventName = (eventName: string) => {
+// };
+// const Event_changeEventLength = (eventName: string) => {
+// };
+
+// const Timetable_addEvent = (eventName: string, row: number, column: number) => {
+// };
+// const Timetable_removeEvent = (eventName: string) => {
+// };
+// const Timetable_moveEvent = (eventName: string, row: number, column: number) => {
+// };
--- src_server/server.ts
+++ src_server/server.ts
@@ -38,31 +38,6 @@ const wrap = (
 const test = () => {
 }
 
-/*
-
-interface Response {
-    succeeded: boolean,
-    message: string | null
-}
-
-const Timetable_addEvent = (eventName: string, row: number, column: number) => {
-};
-const Timetable_removeEvent = (eventName: string) => {
-};
-const Timetable_moveEvent = (eventName: string, row: number, column: number) => {
-};
-
-const Event_newEvent = (eventName: string) => {
-};
-const Event_deleteEvent = (eventName: string) => {
-};
-const Event_changeEventName = (eventName: string) => {
-};
-const Event_changeEventLength = (eventName: string) => {
-};
-
-*/
-
 class Database {
     constructor() {
         // バージョン情報を残しておきたい...
